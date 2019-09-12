3de8c97053268a2091065354ef21c2f320799aa7
Walter Berggren <walter.berggren@gmail.com>
2019-09-12 13:25:20 +0300

    Center layout content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -24,24 +24,35 @@ const Layout: FC = ({ children }) => {
   `)
 
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Header />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "768px",
+            margin: "32px",
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
