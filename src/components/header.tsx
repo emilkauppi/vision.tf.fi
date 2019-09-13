4294b8cfea8d205e0b7d140e0fae2b9677a3fab1
Walter Berggren <walter.berggren@gmail.com>
2019-09-13 10:51:50 +0300

    Set header border color to brick blue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ const Header: FC = () => (
       display: "flex",
       justifyContent: "center",
       padding: "8px 32px 4px 26px",
-      borderBottom: "1px solid lightgray",
+      borderBottom: "1px solid #F1F5FB",
     }}
   >
     <div
