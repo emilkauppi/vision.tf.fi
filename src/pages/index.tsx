6cc6ae43c08b31998719018c407dbc64bd515707
Walter Berggren <walter.berggren@gmail.com>
2020-09-01 00:04:41 +0300

    Remove soft redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,69 @@
-import React, { useEffect } from "react"
+import React, { FC } from "react"
+import { graphql } from "gatsby"
 
-const IndexPage: React.FC = () => {
-  redirectToTFDotFIOnLoad()
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Section, { SectionNode } from "../components/section"
 
-  return <></>
+export const query = graphql`
+  query MyQuery {
+    page: contentfulPage(slug: { eq: "index" }) {
+      section {
+        ... on ContentfulImage {
+          id
+          slug
+          image {
+            file {
+              url
+            }
+          }
+        }
+        ... on ContentfulSection {
+          id
+          title
+          childContentfulSectionBodyTextNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+        ... on ContentfulStatement {
+          id
+          title
+          author
+        }
+        ... on ContentfulVideo {
+          id
+          video {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Query {
+  page: {
+    section: SectionNode[]
+  }
 }
 
-const redirectToTFDotFIOnLoad = () =>
-  useEffect(() => {
-    window.location.href = "https://www.teknologforeningen.fi/"
-  })
+class IndexPage extends React.Component<{ data: Query }> {
+  render() {
+    const sections = this.props.data.page.section.map(section => (
+      <Section key={section.id} title={section.title} node={section} />
+    ))
+
+    return (
+      <Layout>
+        <SEO title="Home" />
+        {sections}
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
