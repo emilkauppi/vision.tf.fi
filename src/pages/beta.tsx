30aad3b30f01cb4261c079660fb2f3ffee9a0c5d
Walter Berggren <walter.berggren@gmail.com>
2020-08-31 23:59:39 +0300

    Add /beta page

---
+++ src/pages/beta.tsx
@@ -0,0 +1,69 @@
+import React, { FC } from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Section, { SectionNode } from "../components/section"
+
+export const query = graphql`
+  query MyQuery {
+    page: contentfulPage(slug: { eq: "index" }) {
+      section {
+        ... on ContentfulImage {
+          id
+          slug
+          image {
+            file {
+              url
+            }
+          }
+        }
+        ... on ContentfulSection {
+          id
+          title
+          childContentfulSectionBodyTextNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+        ... on ContentfulStatement {
+          id
+          title
+          author
+        }
+        ... on ContentfulVideo {
+          id
+          video {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Query {
+  page: {
+    section: SectionNode[]
+  }
+}
+
+class BetaPage extends React.Component<{ data: Query }> {
+  render() {
+    const sections = this.props.data.page.section.map(section => (
+      <Section key={section.id} title={section.title} node={section} />
+    ))
+
+    return (
+      <Layout>
+        <SEO title="Home" />
+        {sections}
+      </Layout>
+    )
+  }
+}
+
+export default BetaPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,76 +1,14 @@
-import React, { FC } from "react"
-import { graphql } from "gatsby"
+import React, { useEffect } from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Section, { SectionNode } from "../components/section"
+const IndexPage: React.FC = () => {
+  redirectToTFDotFIOnLoad()
 
-export const query = graphql`
-  query MyQuery {
-    page: contentfulPage(slug: { eq: "index" }) {
-      section {
-        ... on ContentfulImage {
-          id
-          slug
-          image {
-            file {
-              url
-            }
-          }
-        }
-        ... on ContentfulSection {
-          id
-          title
-          childContentfulSectionBodyTextNode {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-        ... on ContentfulStatement {
-          id
-          title
-          author
-        }
-        ... on ContentfulVideo {
-          id
-          video {
-            file {
-              url
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-interface Query {
-  page: {
-    section: SectionNode[]
-  }
+  return <></>
 }
 
-class IndexPage extends React.Component<{ data: Query}> {
-  componentDidMount() {
+const redirectToTFDotFIOnLoad = () =>
+  useEffect(() => {
     window.location.href = "https://www.teknologforeningen.fi/"
-  }
-  render() {
-    const sections = this.props.data.page.section.map(section => (
-      <Section key={section.id} title={section.title} node={section} />
-    ))
-
-    window.location.href = "https://www.teknologforeningen.fi"
-
-    return (
-      <>
-      </>
-      // <Layout>
-      //   <SEO title="Home" />
-      //   {sections}
-      // </Layout>
-    )
-  }
-}
+  })
 
 export default IndexPage
