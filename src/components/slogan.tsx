7175cbbb2f406fd0b46feb7af7d058a715428562
Walter Berggren <walter.berggren@gmail.com>
2020-10-26 21:51:02 +0200

    Optionalize leading and trailing texts in slogans

--- src/components/slogan.tsx
+++ src/components/slogan.tsx
@@ -2,21 +2,23 @@ import React from "react"
 import styles from "./slogan.module.css"
 
 const Slogan: React.FC<SloganProps> = ({
-    leadingText,
-    boldedText,
-    trailingText
+  leadingText,
+  boldedText,
+  trailingText,
 }) => (
+  <div className={styles.container}>
     <div className={styles.slogan}>
-        <p>{leadingText}</p>
-        <h1>{boldedText}</h1>
-        <p>{trailingText.trailingText}</p>
+      {leadingText && <p>{leadingText}</p>}
+      <h2>{boldedText}</h2>
+      {trailingText && <p>{trailingText.trailingText}</p>}
     </div>
+  </div>
 )
 
 export interface SloganProps {
   boldedText: string
-  leadingText: string
-  trailingText: {
+  leadingText?: string
+  trailingText?: {
     trailingText: string
   }
 }
