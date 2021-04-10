156fd43e8e37895c7b5f762aa01a8db11396e9d3
Walter Berggren <walter.berggren@reaktor.com>
2021-04-10 13:20:43 +0300

    Use PostgreSQL in production

--- donationdb/donationdb/settings.py
+++ donationdb/donationdb/settings.py
@@ -75,13 +75,14 @@ WSGI_APPLICATION = 'donationdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+if DEBUG == True:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
+else:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
