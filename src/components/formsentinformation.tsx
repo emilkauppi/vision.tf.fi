42a6dd649845bb64fd17f7205818df6d8e144cc3
Walter Berggren <walter.berggren@reaktor.com>
2021-04-10 16:19:56 +0300

    Expand the thank you for donating text

--- src/components/formsentinformation.tsx
+++ src/components/formsentinformation.tsx
@@ -21,7 +21,10 @@ const FormSentInformation: React.FC<{
     <div className={styles.container}>
         <h1>Tack för ditt stöd!</h1>
         <p>
-            Gåvobrevet har skickats till den angivna e-postadressen. Klicka <a href="#" onClick={saveForm}>här</a> för att ladda ner en kopia.
+          Gåvobrevet har skickats till den angivna e-postadressen. Klicka <a href="#" onClick={saveForm}>här</a> för att ladda ner en kopia. Kolla din spamkorg ifall du inte hittar meddelandet.
+        </p>
+        <p>
+          Teknologföreningen tackar dig och kommer aldrig att glömma ditt bidrag till nationens framtid. Med din donation försäkrar du att nationens bästa dagar är framför oss, för framtidens teknologer. Du förverkligar något ovärderligt med din donation.
         </p>
         <p>
             Frågor? Kontakta Teknologföreningens fundraisingchef Emil Kauppi (<a href="mailto:funchef@tf.fi">funchef.tf.fi</a>)
