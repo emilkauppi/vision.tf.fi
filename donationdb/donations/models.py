2a92f24b5638f65e2fe5c402ce10e61b92a6f49f
Walter Berggren <walter@berggren.dev>
2021-05-22 15:08:40 +0300

    Add describing string representation for Donation model

--- donationdb/donations/models.py
+++ donationdb/donations/models.py
@@ -40,3 +40,6 @@ class Donation(models.Model):
     # For reference
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}: {self.donation_sum} € ({self.created_at.date()})"
