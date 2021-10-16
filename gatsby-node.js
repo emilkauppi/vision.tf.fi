be11c46c0d5c7c201720053b35c91872f4f19f10
Walter Berggren <walter.berggren@reaktor.com>
2021-10-16 10:53:00 +0300

    Add sharp and canvas dependencies
    Fixes build issues with newer pdfjs versions

--- gatsby-node.js
+++ gatsby-node.js
@@ -152,3 +152,24 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+};
