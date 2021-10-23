c674b9a55c5c35edb6ce8733804228fa411de1b7
Walter Berggren <walter.berggren@reaktor.com>
2021-10-23 15:23:19 +0300

    Remove unneeded xframe_options_exempt

--- donationdb/payments/views.py
+++ donationdb/payments/views.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.clickjacking import xframe_options_exempt
 from .helpers import calculate_hmac, signed_paytrail_headers
 import requests
 
-@xframe_options_exempt
 def providers(request):
     payment_providers_response = requests.get(
         f"{PAYTRAIL_URL}/merchants/payment-providers",
