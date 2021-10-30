0c972767727c6f5c9ab0edfb1b03b3a86a2076f9
Walter Berggren <walter.berggren@reaktor.com>
2021-10-30 13:24:29 +0300

    Remove redundant contribution fields from Donation letter

--- donationdb/donations/models.py
+++ donationdb/donations/models.py
@@ -66,36 +66,16 @@ class DonationLetter(models.Model):
         ["anonymous", "anonymous"]
     ]
 
-    contribution = models.ForeignKey(Contribution, null=True, blank=True, on_delete=models.CASCADE)
-
-    donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
-    organization = models.ForeignKey(
-        Organization,
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE
-    )
-
-    # Donation details 
+    contribution = models.ForeignKey(Contribution, on_delete=models.CASCADE)
     payment_date = models.DateTimeField(blank=True, null=True)
-    donation_sum = models.DecimalField(max_digits=12, decimal_places=2)
-    donation_visibility = models.TextField(choices=VISIBILITY_CHOICES)
-    group_name = models.CharField(max_length=50, blank=True)
-    greeting = models.TextField(blank=True)
     is_paid = models.BooleanField(default=False)
-
-    # Generated donation letter as PDF
     link_to_pdf = models.URLField(blank=True, max_length=2000)
 
-    # Form data as JSON
-    form_data = models.JSONField(blank=True, null=True)
-
-    # For reference
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         organization_or_name = \
-            f"{self.organization.name}: {self.donation_sum} € ({self.created_at.date()})" if (self.organization is not None) \
-            else f"{self.donor.name}: {self.donation_sum} € ({self.created_at.date()})"
+            f"{self.contribution.organization.name}: {self.contribution.sum} € ({self.created_at.date()})" if (self.contribution.organization is not None) \
+            else f"{self.contribution.donor.name}: {self.contribution.sum} € ({self.created_at.date()})"
         return organization_or_name
