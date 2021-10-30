78433d0b94eb21b996968b037a8189942dbfb7f0
Walter Berggren <walter.berggren@reaktor.com>
2021-10-30 12:51:57 +0300

    Revert "Duplicate Donation-specific data from DonationLetter"
    This reverts commit 98796b89c79736a32d38ca4d3692bc19cc80fa1d.

--- donationdb/donations/admin.py
+++ donationdb/donations/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Donation, DonationLetter, Donor, Organization
+from .models import DonationLetter, Donor, Organization
 
 # Register your models here.
-admin.site.register(Donation)
 admin.site.register(DonationLetter)
 admin.site.register(Donor)
 admin.site.register(Organization)
