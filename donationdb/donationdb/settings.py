ce01447a87088a3b64daa525f17024b8118d2181
Walter Berggren <walter.berggren@reaktor.com>
2021-10-30 13:09:19 +0300

    Use postgresql locally too
    Ensure same behavior in development and production environment

--- donationdb/donationdb/settings.py
+++ donationdb/donationdb/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'donationdb.wsgi.application'
 
 DATABASES = {}
 if DEBUG == True:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
