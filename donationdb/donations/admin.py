5ed4c95a1a079d8d834e8a89ea02771192e5fb64
Walter Berggren <walter.berggren@reaktor.com>
2021-10-30 13:10:21 +0300

    Duplicate general donation data to Contribution
    Same as 98796b89c79736a32d38ca4d3692bc19cc80fa1d, but with a different
    name. PostgreSQL failed to apply the previous migration as it was unable
    to create an index for one of the fields due to it already existing.
    It seems like the issue was caused by DonationLetter previously being called
    Donation. Worked around by naming the model Contribution instead.

--- donationdb/donations/admin.py
+++ donationdb/donations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import DonationLetter, Donor, Organization
+from .models import Contribution, DonationLetter, Donor, Organization
 
 # Register your models here.
+admin.site.register(Contribution)
 admin.site.register(DonationLetter)
 admin.site.register(Donor)
 admin.site.register(Organization)
--- donationdb/donations/models.py
+++ donationdb/donations/models.py
@@ -31,11 +31,42 @@ class Organization(models.Model):
         return self.name
 
 
+class Contribution(models.Model):
+    VISIBILITY_CHOICES = [
+        ["visible", "visible"],
+        ["anonymous", "anonymous"]
+    ]
+
+    donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
+    organization = models.ForeignKey(
+        Organization,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+
+    visibility = models.TextField(choices=VISIBILITY_CHOICES)
+    sum = models.DecimalField(max_digits=12, decimal_places=2)
+    greeting = models.TextField(blank=True)
+    group_name = models.CharField(max_length=50, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        organization_or_name = \
+            f"{self.organization.name}: {self.sum} € ({self.created_at.date()})" if (self.organization is not None) \
+            else f"{self.donor.name}: {self.sum} € ({self.created_at.date()})"
+        return organization_or_name
+
+
 class DonationLetter(models.Model):
     VISIBILITY_CHOICES = [
         ["visible", "visible"],
         ["anonymous", "anonymous"]
     ]
+
+    contribution = models.ForeignKey(Contribution, null=True, blank=True, on_delete=models.CASCADE)
 
     donor = models.ForeignKey(Donor, on_delete=models.CASCADE)
     organization = models.ForeignKey(
