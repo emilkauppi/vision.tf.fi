3bcdd2896c8280186c1ad112d2961faa6da5e452
Walter Berggren <walter.berggren@reaktor.com>
2021-12-01 19:12:28 +0200

    Use SQLite in CI tests for now

--- donationdb/donationdb/settings.py
+++ donationdb/donationdb/settings.py
@@ -93,7 +93,12 @@ WSGI_APPLICATION = 'donationdb.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {}
-if DEBUG == True:
+if CI == True:
+    DATABASES['default'] = {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+elif DEBUG == True:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
