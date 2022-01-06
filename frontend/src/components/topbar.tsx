bff2b09a72b1469b6ba26ef8f1f5020b52d1253d
Walter Berggren <walter@berggren.dev>
2022-01-06 10:29:29 +0200

    Hide link to payments page behind feature flag

--- frontend/src/components/topbar.tsx
+++ frontend/src/components/topbar.tsx
@@ -7,26 +7,31 @@ import TFLogo from "./tflogo"
 const TopBar: React.FC<{
   title?: string
   isDonateButtonHidden?: boolean
-}> = ({ title, isDonateButtonHidden }) => (
-  <div className={styles.container}>
-    <nav className={styles.topbar}>
-      <div className={styles.currentPage}>
-        <Link to="/">
-          <motion.span className={styles.logo} whileHover={{ scale: 1.1 }}>
-            <TFLogo variant="white" />
-          </motion.span>
-        </Link>
-        {title && <span>{title}</span>}
-      </div>
-      {!isDonateButtonHidden && (
-        <motion.div whileHover={{ scale: 1.1 }}>
-          <Link to="/stod-projektet" className={styles.donate}>
-            Stöd projektet
+}> = ({ title, isDonateButtonHidden }) => {
+  const isLinkToPaymentsVisible = process.env.GATSBY_LINK_TO_PAYMENTS_VISIBLE === "True"
+  const paymentsPageOrContactPage = isLinkToPaymentsVisible ? "/stod-projektet" : "/kontakt"
+  const supportOrSubscribe = isLinkToPaymentsVisible ? "Stöd projektet" : "Hör mer"
+  return (
+    <div className={styles.container}>
+      <nav className={styles.topbar}>
+        <div className={styles.currentPage}>
+          <Link to="/">
+            <motion.span className={styles.logo} whileHover={{ scale: 1.1 }}>
+              <TFLogo variant="white" />
+            </motion.span>
           </Link>
-        </motion.div>
-      )}
-    </nav>
-  </div>
-)
+          {title && <span>{title}</span>}
+        </div>
+        {!isDonateButtonHidden && (
+          <motion.div whileHover={{ scale: 1.1 }}>
+            <Link to={paymentsPageOrContactPage} className={styles.donate}>
+              {supportOrSubscribe}
+            </Link>
+          </motion.div>
+        )}
+      </nav>
+    </div>
+  )
+}
 
 export default TopBar
