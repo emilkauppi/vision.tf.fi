346cbf507fd91e3f7d62e68444a5ffa3cfc8a0cb
Walter Berggren <walter@berggren.dev>
2022-01-11 19:42:02 +0200

    Visualize staging and production deployment state

--- donationdb/deployments/helpers.py
+++ donationdb/deployments/helpers.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timezone
+import dateutil.parser
 import json
+from math import floor
 import requests
 from donationdb.settings import HEROKU_OATH_TOKEN
 
@@ -10,12 +13,65 @@ def latest_releases(heroku_app):
             "Authorization": f"Bearer {HEROKU_OATH_TOKEN}"
         }
     )
-    print(json.loads(response.text))
     newest_releases_first = sorted(
         json.loads(response.text),
         key=lambda release: release["created_at"].lower(),
         reverse=True
     )
-    return newest_releases_first
+    formatted = [
+        {
+            "created_at": dateutil.parser.isoparse(release["created_at"]),
+            "description": release["description"],
+            "time_since": time_since_as_text(dateutil.parser.isoparse(release["created_at"])),
+            "status": release["status"]
+        }
+        for release in newest_releases_first
+    ]
+    return formatted
+
+
+def time_since_as_text(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    Remixed from: https://stackoverflow.com/questions/1551382/user-friendly-time-format-in-python
+    """
+    now = datetime.now(timezone.utc)
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "nyss"
+        if second_diff < 60:
+            return str(second_diff) + " sekunder sedan"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(floor(second_diff / 60)) + " minuter sedan"
+        if second_diff < 7200:
+            return "en timme sedan"
+        if second_diff < 86400:
+            return str(floor(second_diff / 3600)) + " timmar sedan"
+    if day_diff == 1:
+        return "Igår"
+    if day_diff < 7:
+        return str(day_diff) + " dagar sedan"
+    if day_diff < 31:
+        return str(floor(day_diff / 7)) + " veckor sedan"
+    if day_diff < 365:
+        return str(floor(day_diff / 30)) + " månder sedan"
+    return str(day_diff / 365) + " år sedan"
+
 
 HEROKU_URL_BASE = "https://api.heroku.com"
--- donationdb/deployments/views.py
+++ donationdb/deployments/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 import requests
 
 from deployments.helpers import latest_releases
-from donationdb.settings import HEROKU_APP_VISION_TF_FI
+from donationdb.settings import HEROKU_APP_VISION_TF_FI, HEROKU_APP_STAGING_VISION_TF_FI
 
 @login_required
 def index(request):
-    releases = latest_releases(HEROKU_APP_VISION_TF_FI)
+    staging_releases = latest_releases(HEROKU_APP_STAGING_VISION_TF_FI)
+    production_releases = latest_releases(HEROKU_APP_VISION_TF_FI)
     context = {
         "subpage": "Deployments",
-        "releases": releases
+        "production_releases": production_releases,
+        "staging_releases": staging_releases
     }
-    print(context["releases"])
     return render(request, "deployments/index.html", context)
